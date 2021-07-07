e30e0e8f20f12cb332f2a5ae6327944406ff1369
sasidhar <sasidhar741@gmail.com>
2021-07-07 11:41:04 +0530

    Added assets

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +19,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onCPressed(_ sender: UIButton) {
+        
+        playSound(songName: sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    //code that executes after 0.2 seconds of delay
+                    sender.alpha = 1.0
+             }
+    }
+    
+    func playSound(songName: String) {
+           let url = Bundle.main.url(forResource: songName, withExtension: "wav")
+           player = try! AVAudioPlayer(contentsOf: url!)
+           player.play()
+                   
+       }
+    
+    
 }
 
